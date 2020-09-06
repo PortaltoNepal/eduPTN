68ac0a50c568bf32746968eadbe1053bd6dadeea
Alok Shrestha <aalooksth@gmail.com>
2020-09-06 14:23:18 +0000

    Updated 'sw.js' via CloudCannon

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', function(e) {
  e.waitUntil(
-   caches.open('fox-store').then(function(cache) {
+   caches.open('eduPTN').then(function(cache) {
      return cache.addAll([
          '/',
        '/notes/'
