8af3ed7af140ce539b5029b35901bea7fc97686d
Alok Shrestha <aalooksth@gmail.com>
2020-09-08 05:56:41 +0000

    Updated 'assets/custom.js' via CloudCannon

--- assets/custom.js
+++ assets/custom.js
@@ -1,5 +1,5 @@
 // Try using right-click on the preview page. This script disables that functionality.
 window.addEventListener('contextmenu', function (e) {
-  document.body.innerHTML += '<p>Right-click is disabled</p>'
+  console.log('<p>Right-click is disabled</p>')
   e.preventDefault();
 }, false);
